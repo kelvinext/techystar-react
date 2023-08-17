96104bb3d9ec64af41990bd88b95b401406afd20
Kelvin <kelvinpradhan18@gmail.com>
2023-08-17 08:57:20 +0530

    Updates

---
+++ precache-manifest.76602f4a6e0b1202ce8a5da8d1fab224.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "13e5484fb1c8b9973267",
+    "url": "/static/css/main.fb0672c9.chunk.css"
+  },
+  {
+    "revision": "13e5484fb1c8b9973267",
+    "url": "/static/js/main.13e5484f.chunk.js"
+  },
+  {
+    "revision": "0ed09f928108f9d89cf4",
+    "url": "/static/css/1.29db635d.chunk.css"
+  },
+  {
+    "revision": "0ed09f928108f9d89cf4",
+    "url": "/static/js/1.0ed09f92.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "48ac8421f6441093695d5865e1fc768d",
+    "url": "/static/media/vg.48ac8421.png"
+  },
+  {
+    "revision": "b1e9050378e482b7a4dbbfc880041c83",
+    "url": "/index.html"
+  }
+];
