08d0ee41f4bd61bba56c3397a2fe5363542f43d8
Kelvin <kelvinpradhan18@gmail.com>
2023-08-17 09:13:09 +0530

    Updates

---
+++ precache-manifest.a724eb7a4bdd2a2e1acf3b7eef55917f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "13e5484fb1c8b9973267",
+    "url": "/techystar-react/static/css/main.fb0672c9.chunk.css"
+  },
+  {
+    "revision": "13e5484fb1c8b9973267",
+    "url": "/techystar-react/static/js/main.13e5484f.chunk.js"
+  },
+  {
+    "revision": "0ed09f928108f9d89cf4",
+    "url": "/techystar-react/static/css/1.29db635d.chunk.css"
+  },
+  {
+    "revision": "0ed09f928108f9d89cf4",
+    "url": "/techystar-react/static/js/1.0ed09f92.chunk.js"
+  },
+  {
+    "revision": "43630f26e5d65b35ee9d",
+    "url": "/techystar-react/static/js/runtime~main.43630f26.js"
+  },
+  {
+    "revision": "48ac8421f6441093695d5865e1fc768d",
+    "url": "/techystar-react/static/media/vg.48ac8421.png"
+  },
+  {
+    "revision": "d7ef1002406145b8cfdb077a89def6ac",
+    "url": "/techystar-react/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.76602f4a6e0b1202ce8a5da8d1fab224.js"
+  "/techystar-react/precache-manifest.a724eb7a4bdd2a2e1acf3b7eef55917f.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/techystar-react/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
